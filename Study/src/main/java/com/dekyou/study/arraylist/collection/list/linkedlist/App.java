3dcc51b3eeaf9d7d1dff4053c2411f0563a52562
Dekyou <dekyous@gmail.com>
2018-05-18 18:40:28 +0800

    新的测试

---
+++ Study/src/main/java/com/dekyou/study/arraylist/collection/list/linkedlist/App.java
@@ -0,0 +1,17 @@
+package com.dekyou.study.arraylist.collection.list.linkedlist;
+
+import java.util.LinkedList;
+
+/**
+ * @author GaoJing
+ * @date 2018年5月14日
+ * 
+ */
+public class App {
+
+	public void test() {
+		LinkedList<String> linkedList = new LinkedList<String>();
+		linkedList.add("");
+	}
+}
+
